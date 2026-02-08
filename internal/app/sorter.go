4b99fd8d95d1f9de87c0fd7bb15daeb7d853973e
pawwwline <k.paulieee@gmail.com>
2026-02-08 16:36:30 +0500

    refactor(sorter): add describing comments

--- internal/app/sorter.go
+++ internal/app/sorter.go
@@ -5,6 +5,8 @@ import (
 	"io"
 )
 
+// Sorter defines the contract for implementing various sorting algorithms.
 type Sorter interface {
+	// Sort reads data from r, performs sorting, and writes the result to w.
 	Sort(ctx context.Context, r io.Reader, w io.Writer) error
 }
