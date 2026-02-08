a5df9088cd8ef04d93933b1f7803d08f8bd43c08
pawwwline <k.paulieee@gmail.com>
2026-02-08 13:55:02 +0500

    feat(app): implement Run method for orchestrating sorting functionality

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,1 +1,20 @@
 package app
+
+import (
+	"context"
+	"io"
+)
+
+type App struct {
+	sorter Sorter
+}
+
+func New(sorter Sorter) *App {
+	return &App{
+		sorter: sorter,
+	}
+}
+
+func (a *App) Run(ctx context.Context, r io.Reader, w io.Writer) error {
+	return a.sorter.Sort(ctx, r, w)
+}
