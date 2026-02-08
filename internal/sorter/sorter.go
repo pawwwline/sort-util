c91d4d2eb06097b80012374748a4d9156a4bbe4c
pawwwline <k.paulieee@gmail.com>
2026-02-08 16:35:36 +0500

    refactor(sorter): Add error handling and describing comments

--- internal/sorter/sorter.go
+++ internal/sorter/sorter.go
@@ -1,7 +1,9 @@
+// Package sorter provides implementations of various sorting strategies.
 package sorter
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"slices"
 
@@ -9,38 +11,41 @@ import (
 	"sort-util/internal/provider"
 )
 
+// InMemory provides an implementation of the Sorter interface that processes data in RAM.
 type InMemory struct {
 	cfg config.Options
 }
 
+// NewInMemory initializes a new InMemory sorter with the provided configuration options.
 func NewInMemory(cfg config.Options) *InMemory {
 	return &InMemory{cfg: cfg}
 }
 
-func (i *InMemory) Sort(ctx context.Context, r io.Reader, w io.Writer) error {
-	strings, err := provider.ReadLines(ctx, r)
+// Sort in memory implementation with config flag handling.
+func (i *InMemory) Sort(ctx context.Context, reader io.Reader, writer io.Writer) error {
+	lines, err := provider.ReadLines(ctx, reader)
 	if err != nil {
-		return err
+		return fmt.Errorf("read lines: %w", err)
 	}
 
-	i.sortLines(strings) //sort lines in place
+	i.sortLines(lines) // sort lines in place
 
 	if i.cfg.Unique {
-		strings = uniqueLines(strings)
+		lines = uniqueLines(lines)
 	}
 
-	err = provider.WriteLines(ctx, w, strings)
+	err = provider.WriteLines(ctx, writer, lines)
 	if err != nil {
-		return err
+		return fmt.Errorf("write lines: %w", err)
 	}
 
 	return nil
 }
 
-func (i *InMemory) sortLines(sortedStrings []string) {
+func (i *InMemory) sortLines(sortedLines []string) {
 	compare := newComparator(i.cfg)
 
-	slices.SortFunc(sortedStrings, func(a, b string) int {
+	slices.SortFunc(sortedLines, func(a, b string) int {
 		if compare(a, b) {
 			return -1 // a is less
 		}
