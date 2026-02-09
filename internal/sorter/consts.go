210b311bfd4730a5ac462aa08061e7e3faeff872
pawwwline <k.paulieee@gmail.com>
2026-02-09 20:56:32 +0500

    feat(types): add months const

---
+++ internal/sorter/consts.go
@@ -0,0 +1,19 @@
+package sorter
+
+type months int
+
+const (
+	_                = iota
+	january   months = iota
+	february  months = iota
+	march     months = iota
+	april     months = iota
+	may       months = iota
+	june      months = iota
+	july      months = iota
+	august    months = iota
+	september months = iota
+	october   months = iota
+	november  months = iota
+	december  months = iota
+)
