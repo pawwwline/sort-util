e94c4ce952b0db962911909da54f7ab966ca4827
pawwwline <k.paulieee@gmail.com>
2026-02-09 18:50:10 +0500

    refactor(core): refactor App struct and New function
    Introduce config.Options in App struct and New function for better flexibility.

--- internal/app/app.go
+++ internal/app/app.go
@@ -5,23 +5,37 @@ import (
 	"context"
 	"fmt"
 	"io"
+
+	"sort-util/internal/config"
+	"sort-util/internal/sorter"
 )
 
 // App represents the core application responsible for orchestrating the sorting process.
 type App struct {
-	sorter Sorter
+	cfg config.Options
 }
 
 // New initializes and returns a new App instance with the provided Sorter implementation.
-func New(sorter Sorter) *App {
+func New(cfg config.Options) *App {
 	return &App{
-		sorter: sorter,
+		cfg: cfg,
 	}
 }
 
 // Run orchestrates the sorting process by passing the reader and writer to the underlying sorter.
 func (a *App) Run(ctx context.Context, r io.Reader, w io.Writer) error {
-	if err := a.sorter.Sort(ctx, r, w); err != nil {
+	if a.cfg.CheckSorted {
+		checker := sorter.NewChecker(a.cfg)
+		err := checker.CheckSorted(ctx, r)
+		if err != nil {
+			return fmt.Errorf("check sorting: %w", err)
+		}
+
+		return nil
+	}
+
+	inMemSorter := sorter.NewInMemory(a.cfg)
+	if err := inMemSorter.Sort(ctx, r, w); err != nil {
 		return fmt.Errorf("sort input: %w", err)
 	}
 
